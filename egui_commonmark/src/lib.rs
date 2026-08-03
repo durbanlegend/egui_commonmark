41acaa0543ad1546c4ea7a901663c9ccadaac781
durbanlegend <dforbes@iafrica.com>
2026-08-03 17:28:47 +0200

    Reinstate scroll example

--- egui_commonmark/src/lib.rs
+++ egui_commonmark/src/lib.rs
@@ -310,7 +310,7 @@ impl<'f> CommonMarkViewer<'f> {
     ///
     /// [`ScrollArea`]: egui::ScrollArea
     /// [`show`]: crate::CommonMarkViewer::show
-    #[doc(hidden)] // Buggy in scenarios more complex than the example application
+    // #[doc(hidden)] // Buggy in scenarios more complex than the example application
     #[cfg(feature = "pulldown_cmark")]
     pub fn show_scrollable(
         self,
